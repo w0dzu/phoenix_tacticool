d24f9146ea9c8a8ca427b89d5dae65ec67e8c890
Dawid P <dawid.paszkot98@gmail.com>
2023-02-02 12:14:12 +0100

    Added user/admin bar that will be displaed after sucessful login

---
+++ admin/adminbar.php
@@ -0,0 +1,20 @@
+<nav class="adminbar">
+    <ul class="ab-toolbar">
+        <li><a class="logo" href="#"><img src="<?php echo $url . "images/phoenix-tactical-logo.png"?>" alt="LG"></a></li>
+        <li><a href="#">Dodaj misje</a></li>
+        <li><a href="#">Dodaj trening</a></li>
+    </ul>
+    <div class="btn">
+        <a href="#">
+            <p>Witaj, <b>Username</b></p><img src="<?php echo $url . "uploads/avatar.png"?>" alt="AV">
+        </a>
+        <div class="user-profile">
+            <img src="<?php echo $url . "uploads/avatar.png"?>" alt="AV">
+            <ul>
+                <li><a href="#">Username</a></li>
+                <li><a href="#">Edit profile</a></li>
+                <li><a href="<?php echo $url . "includes/logout.inc.php"?>">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- components/header.php
+++ components/header.php
@@ -1,10 +1,10 @@
 <header>
     <div class="header">
         <div class="logo">
-            <a href="#"><img src="images/phoenix-tactical-logo.png" alt="LOGO"></a>
+            <a href="#"><img src="<?php echo $url . "images/phoenix-tactical-logo.png"?>" alt="LOGO"></a>
         </div>
         <div class="title">
-            <a href="#"><p>PHOENIX TACTICAL</p></a>
+            <a href="#"><p>PHOENIX TACTICOOL</p></a>
         </div>
         <div class="dropdown"><a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a></div>
     </div>
