599bcc8b444f9afe4740b6dd17f8c8f8fe2f1986
Dawid P <dawid.paszkot98@gmail.com>
2023-02-07 11:05:31 +0100

    Small fix to adminbar.php

--- admin/adminbar.php
+++ admin/adminbar.php
@@ -1,17 +1,23 @@
+<?php 
+    $user_uuid = $_SESSION['user_uuid'];
+    $user_login = $_SESSION['user_login'];
+
+?>
+
 <nav class="adminbar">
     <ul class="ab-toolbar">
-        <li><a class="logo" href="#"><img src="<?php echo $url . "images/phoenix-tactical-logo.png"?>" alt="LG"></a></li>
+        <li><a class="logo" href="#"><img src="http://localhost/phoenix_tacticool/images/phoenix-tactical-logo.png" alt="LG"></a></li>
         <li><a href="#">Dodaj misje</a></li>
         <li><a href="#">Dodaj trening</a></li>
     </ul>
     <div class="btn">
         <a href="#">
-            <p>Witaj, <b>Username</b></p><img src="<?php echo $url . "uploads/avatar.png"?>" alt="AV">
+            <p>Witaj, <b><?php echo $user_login; ?></b></p><img src="http://localhost/phoenix_tacticool/uploads/avatar.png" alt="AV">
         </a>
         <div class="user-profile">
-            <img src="<?php echo $url . "uploads/avatar.png"?>" alt="AV">
+            <img src="http://localhost/phoenix_tacticool/uploads/avatar.png" alt="AV">
             <ul>
-                <li><a href="#">Username</a></li>
+                <li><a href="#"><?php echo $user_login; ?></a></li>
                 <li><a href="#">Edit profile</a></li>
                 <li><a href="includes/logout.inc.php">Logout</a></li>
             </ul>
